ba7e95b40ad530f560e4b6a130de3227d0577514
takakazu.yanaka <takakazu.yanaka@datahotel.co.jp>
2017-07-26 11:54:00 +0900

    customer_api_ok

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #docker-compose run zabbix_api django-admin.py startproject zabbix_api .
-docker-compose run customer_api django-admin.py startproject customer_api .
-#docker-compose run table django-admin.py startproject table .
+#docker-compose run customer_api django-admin.py startproject customer_api .
+docker-compose run table django-admin.py startproject table .
 #docker-compose run graph django-admin.py startproject graph .
 
