1fde4b1e9579de221a3dae65076970ec91ec3ef7
t-yanaka <nmz314.lnx@gmail.com>
2017-09-02 02:38:23 +0900

    ajax-ok

---
+++ nginx/html/js/zabbix2.js
@@ -0,0 +1,52 @@
+$(function(){
+$('#ajax-button').submit(function(event) {
+    // HTMLでの送信をキャンセル
+    event.preventDefault();
+    
+    // 操作対象のフォーム要素を取得
+    var $form = $(this);
+    var textarea1 = $("#textarea-id").val(); 
+    // 送信ボタンを取得
+    var $button = $form.find('button');
+    
+    var url = "http://report.com/zabbix/";
+    var requestParam = {"host":"10.0.1.163", "port":3306, "db":"zabbix", "user":"zabbix", "passwd":"zabbix", "charset":"utf8", "query": textarea1};
+    //alert(JSON.stringify(requestParam));
+    
+    $.ajax({
+                type : 'POST',
+                url : url,
+                data : JSON.stringify(requestParam),
+                //data : requestParam,
+                dataType : 'JSON',
+                contentType:'application/json',
+                scriptCharset: 'UTF-8',
+                timeout : 300000000,
+                success : function(data) {
+                    var d = JSON.stringify(data);
+                    //$("#table").html(data);
+                    //$("#table").append(d);
+                    // Success
+                    //alert(d);
+                    //document.write(d);
+                    //document.getElementById('table').appendChild(d);
+                    buf = "<table border=1 style='color:blue'>";
+                    for (var i = 0; i < data.length; i++) {
+                        buf=buf + "<tr>";
+                        var keys = Object.keys(data[i])
+                        for (var j = 0; j < keys.length; j++) {
+                            buf=buf + "<td>"+ data[i] [keys[j]] +"</td>";
+                        }
+                        buf=buf + "</tr>";   
+                    }
+                    buf = buf+"</table>";
+                    $("#table").html(buf); 
+                },
+                error : function(data) {
+                    var d = JSON.stringify(data);
+                    // Error
+                    alert(d);
+                }
+            });
+});
+});
